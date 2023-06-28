f7ab0870549a0bd4a55a0ddf1715a9285dc4ef9c
Ralph Shui <rskc.2112@gmail.com>
2023-06-28 11:10:16 -0400

    completing liker lab

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+heart = document.getElementsByClassName("like-glyph");
+errorMsg = document.getElementById("modal");
 
-
-
+for (let i = 0; i < heart.length; i++) {
+  heart[i].addEventListener("click", () => {
+    if(heart[i].textContent === FULL_HEART) {
+      heart[i].textContent = EMPTY_HEART;
+      heart[i].classList.remove("activated-heart")
+      setTimeout(() => {alert("Successlly Uniked!")}, 100)
+    }else mimicServerCall()
+          .then(() => {
+            heart[i].textContent = FULL_HEART
+            heart[i].classList.add("activated-heart")
+            setTimeout(() => {alert("Successlly Liked!")}, 100)
+          })
+          .catch(() => {
+            errorMsg.classList.remove("hidden");
+            setTimeout(() => errorMsg.classList.add("hidden"), 3000);
+          })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
